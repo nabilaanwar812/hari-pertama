2998c1f6e492d64b21ad6be45ff3446893729df1
nabila <inxnabila812@gmail.com>
2019-03-13 13:23:49 +0700

    file pertama

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Gambar from './component/Gambar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Hallo nama saya Nabila</h2>
+        <Gambar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/component/Gambar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Gambar extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Saya Hobi Main game</h2>
+            </div>
+        )
+    }
+}
